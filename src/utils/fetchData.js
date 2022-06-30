ad9a18a0633672b1725c901cf1bdbddf66d7ce59
Sandeep Thakur <sandeep@Sandeeps-MacBook-Air.local>
2022-06-30 14:24:19 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,21 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "3c3b0c6946msheaf3bf044fc0079p1dea54jsnb0d28e51efb3",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "3c3b0c6946msheaf3bf044fc0079p1dea54jsnb0d28e51efb3",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
